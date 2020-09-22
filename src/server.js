0efa64c4c82eff7e3e48050775f31368efe352be
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2020-09-22 23:40:40 +0200

    upvoting articles

--- src/server.js
+++ src/server.js
@@ -2,6 +2,19 @@ import express from "express";
 // for the post we need and extra module
 import bodyParser from "body-parser";
 
+// fake database (not persistent I imagine)
+const articlesInfo = {
+  "learn-react": {
+    upvotes: 0,
+  },
+  "learn-node": {
+    upvotes: 0,
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0,
+  },
+};
+
 //  we create our backend app like this
 const app = express();
 
@@ -9,12 +22,29 @@ const app = express();
 app.use(bodyParser.json());
 
 // endpoint for hello
+app.get("/", (req, res) => res.send("HOME"));
+app.get("/test", (req, res) =>
+  res.send("Tulio estamos al aire (lease con voz de Juanin) !")
+);
 // GET
-app.get("/hello", (req, res) => res.send("Hello get!"));
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}!`));
+// app.get("/hello", (req, res) => res.send("Hello get!"));
+// app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}!`));
 // POST
 // app.post("/hello", (req, res) => res.send("POST"));
-app.post("/hello", (req, res) => res.send(`Hello ${req.body.name} ${req.body.last} !`));
+// app.post("/hello", (req, res) =>
+//   res.send(`Hello ${req.body.name} ${req.body.last} !`)
+// );
+
+// new endpoint
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const articleName = req.params.name;
+  articlesInfo[articleName].upvotes += 1;
+  res
+    .status(200)
+    .send(
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+    );
+});
 
 // start the backend-server
 app.listen(8000, () => console.log("Listening on Port 8000"));
