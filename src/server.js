1ba508c43b1f42dd3e13e7644f1a41d447483d2c
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2020-09-22 23:06:01 +0200

    testing express server with postman

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 import express from "express";
+// for the post we need and extra module
+import bodyParser from "body-parser";
 
 //  we create our backend app like this
 const app = express();
 
-// endpoint
-app.get("/hello", (req, res) => res.send("Hello back!"));
+// parses the json
+app.use(bodyParser.json());
+
+// endpoint for hello
+// GET
+app.get("/hello", (req, res) => res.send("Hello get!"));
+// POST
+// app.post("/hello", (req, res) => res.send("POST"));
+app.post("/hello", (req, res) => res.send(`Hello ${req.body.name} ${req.body.last} !`));
 
 // start the backend-server
-app.listen(8000, ( ) => console.log('Listening on Port 8000'))
+app.listen(8000, () => console.log("Listening on Port 8000"));
